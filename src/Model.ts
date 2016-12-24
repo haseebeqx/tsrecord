caad61158b0b342db4c5d71503644da35fb6fe29
Haseeb <haseebeqx@yahoo.com>
2016-12-24 18:54:06 +0530

    basic read operation support

--- src/Model.ts
+++ src/Model.ts
@@ -1,6 +1,6 @@
 import {Builder} from "./Builder";
 import {Helper} from "./Helper";
-export class Model{
+export class Model implements IModel{
     save(){
         let helper = new Helper();
         let columns = helper.getParams(this);
@@ -13,4 +13,27 @@ export class Model{
         var result = builder.table(table).insert(insert);
         return result;
     }
+
+     all(callback:(model :  IModel[])=>void){
+        let helper = new Helper();
+        let columns = helper.getParams(this);
+        let table = helper.getTableName(this);
+        let builder = new Builder();
+        builder.table(table).getAll((result)=>{
+            let retArray = [];
+            
+            for(let i=0;i<result.rows.length;i++){
+                let instance = this.constructor();
+                for(let j in result.rows[i]){
+                    instance[j] = result.rows[i][j];
+                }
+                retArray.push(instance);
+            }
+            callback(retArray);
+        });
+    }
+}
+
+export interface IModel{
+    save();
 }
